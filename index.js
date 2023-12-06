615e8831b298fd0601dfb2faa958099a4272f7ac
Muhammad Zubair <muhammad.zubair@gmail.com>
2023-12-06 10:39:21 +0500

    morgan logs

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 // index.js
 const express = require('express');
+const morgan = require('morgan');
 const { mainRoutes } = require('./routes');
 
 // Create an instance of the Express application
 const app = express();
 
+app.use(morgan('dev'));
 // Use the mainRoutes for the root path
-app.use('/', mainRoutes);
+app.use('/main', mainRoutes);
 
 // Make the server listen on port 3000
 const PORT = 3000;
